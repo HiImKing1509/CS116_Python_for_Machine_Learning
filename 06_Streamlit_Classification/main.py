1fbb6ffc50c0b7716b8c5f0ee1d1cb0b5420fd80
Huynh Viet Tuan Kiet <hvtkiet.15092002@gmail.com>
2022-11-05 09:11:04 +0700

    AC

--- 06_Streamlit_Classification/main.py
+++ 06_Streamlit_Classification/main.py
@@ -12,15 +12,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 from styles import styles, text_success
 
 # ===== function =====
-def reset_session_state():
-    st.session_state['trained'] = False 
-    return
 
 # ======================================================================================== Application
 
@@ -103,7 +100,7 @@ if uploaded_file is not None:
     index = 0
     for i in data:
         with cols[index % 5]:
-            st.checkbox(i, key='dynamic_checkbox_' + i, on_change=reset_session_state)
+            st.checkbox(i, key='dynamic_checkbox_' + i)
         index += 1
          
     features_train = data.loc[:, feature_select]
@@ -125,7 +122,6 @@ if uploaded_file is not None:
         selectbox_label = st.radio(
             "",
             tuple(features_not_train),
-            on_change=reset_session_state
         )
         features_label = selectbox_label
     with col2:
@@ -136,17 +132,13 @@ if uploaded_file is not None:
         )
         if selectbox_label != None:
             df1 = data[selectbox_label].value_counts()
+            print(df1)
+            print(type(df1))
             st.bar_chart(df1, use_container_width=True)
             st.dataframe(data.loc[:, features_label].to_frame().style.background_gradient(cmap='bone'), use_container_width=True)
             
     # ============================================ Train model ============================================
-    st.markdown(styles.lines_section_separate_style, unsafe_allow_html=True)
     if selectbox_label != None:
-        st.markdown(
-            """
-                # One-hot Encoder
-            """
-        )
         
         flag_label_is_obj = False
         
@@ -189,17 +181,15 @@ if uploaded_file is not None:
             button_train_test_split = st.selectbox(
                 "",
                 ('Train test split', 'K-Fold'),
-                on_change=reset_session_state
             )
         with col2:
             st.write("")
         with col3:
             if button_train_test_split == 'Train test split':
-                slider_train_test_split = st.slider('Training rate', 0.0, 0.99, 0.8, on_change=reset_session_state)
+                slider_train_test_split = st.slider('Training rate', 0.0, 0.99, 0.8)
                 st.markdown(f"Train size accounts for {slider_train_test_split * 100}% dataset")
                 input_random_state = st.number_input('Random state', min_value=0)
                 st.markdown(f"Random state equal {input_random_state}")
-                X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=slider_train_test_split, random_state=input_random_state)
             else:
                 split_value = st.number_input("", min_value=2)
                 st.write('You selected:', split_value)
@@ -220,14 +210,10 @@ if uploaded_file is not None:
             )
                     
             model = None
+            train = False
             if st.button("Run"):
+                train = True
                 st.session_state['trained'] = True
-                y_train = y_train.reshape(-1)
-                y_test = y_test.reshape(-1)
-                
-                sc = StandardScaler()
-                X_train = sc.fit_transform(X_train)
-                X_test = sc.transform(X_test)
                 
                 if option_model == 'Logistic Regression':
                     model = LogisticRegression()
@@ -239,14 +225,73 @@ if uploaded_file is not None:
                     model = GaussianNB()
                 else:
                     model = RandomForestClassifier()
-                hist = model.fit(X_train, y_train)
-                y_pred = model.predict(X_test)
+                    
+                y_pred_max = None,
+                y_test_max = None
+                if button_train_test_split == "Train test split":
+                    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=slider_train_test_split, random_state=input_random_state)
+                    y_train = y_train.reshape(-1)
+                    y_test = y_test.reshape(-1)
+                    
+                    sc = StandardScaler()
+                    X_train = sc.fit_transform(X_train)
+                    X_test = sc.transform(X_test)
+                    hist = model.fit(X_train, y_train)
+                    y_pred = model.predict(X_test)
+                    acc_score = accuracy_score(y_pred, y_test)
+                    y_pred_max = y_pred.copy()
+                    y_test_max = y_test.copy()
+                else:
+                    i = 0
+                    fold = []
+                    acc_score = 0
+                    
+                    kf = KFold(n_splits = int(split_value))
+                    for train_index, test_index in kf.split(X):
+                        my_bar = st.progress(0)
+                        # Split data
+                        X_train, X_test = X[train_index], X[test_index]
+                        y_train, y_test = y[train_index], y[test_index]
+                        
+                        # k-fold train model
+                        hist = model.fit(X_train, y_train)
+                        
+                        # k-fold test model
+                        y_pred = model.predict(X_test)
+                        
+                        acc_score_pred = accuracy_score(y_pred, y_test)
+                        if acc_score_pred > acc_score:
+                            acc_score = acc_score_pred
+                            y_pred_max = y_pred
+                            y_test_max = y_test
+                            
+                        
+                        st.text(f"{i+1} Fold successfully")
+                        
+                        for percent_complete in range(0, 100, 20):
+                            time.sleep(0.1)
+                            my_bar.progress(percent_complete + 20)
+                        
+                        fold.append(f"Fold {i}")
+                        i += 1
                 
                 st.session_state['model'] = model
                 
-                st.text_input(label="", value=accuracy_score(y_pred, y_test))
+                ps = precision_score(y_test_max, y_pred_max, average='weighted')
+                rs = recall_score(y_test_max, y_pred_max, average='weighted')
+                fs = f1_score(y_test_max, y_pred_max, average='weighted')
+                st.markdown(
+                    f"""
+                        ### Accuracy score: `{acc_score}`\n
+                        ### Precision score: `{ps}`\n
+                        ### Recall score: `{rs}`\n
+                        ### F1 score: `{fs}`\n
+                    """
+                )
+                st.bar_chart(pd.Series([ps, rs, fs], index =['Precision score', 'Recall score', 'F1 score']), use_container_width=True) 
+                
                 with col3:
-                    pred_df = pd.DataFrame({'Actual Value': y_test, 'Predicted Value': y_pred, 'Same': y_test == y_pred})
+                    pred_df = pd.DataFrame({'Actual Value': y_test_max, 'Predicted Value': y_pred_max, 'Same': y_test_max == y_pred_max})
                     # if flag_label_is_obj == True:
                     #     for i in range(0, len(y_pred)):
                     #         pred_df["Actual Value"][i] = mapping_label[i] 
@@ -255,7 +300,7 @@ if uploaded_file is not None:
                     count = [pred_df["Actual Value"][i] == pred_df["Predicted Value"][i] for i in range(0, len(pred_df))].count(True)
                     st.markdown(
                             f"""
-                                ### {count} / {len(pred_df)} samples are predicted exactly
+                                ### `{count} / {len(pred_df)}` samples are predicted exactly
                             """
                     )
         if not st.session_state['trained']:
@@ -294,19 +339,22 @@ if uploaded_file is not None:
                     
                     feature_real.append(enter_state)
             index += 1
-                                
+                       
         if st.button("Make Predict"):
-            # if len(feature_real) == len(features_train.columns):
-                try:
-                    model = st.session_state['model']    
-                    predict_arr = np.array(feature_real)
-                    predict_arr = np.reshape(predict_arr, (1, -1))
-                    predict_arr_ = np.asarray(predict_arr, dtype=float)
-                    y_pred_real = model.predict(predict_arr)
-                    st.write(y_pred_real[0], unsafe_allow_html=True)
-                except Exception as e:
-                    st.write(e)
-            # else:   
-                # st.info("Error")
+            try:
+                model = st.session_state['model']
+                predict_arr = np.array(feature_real)
+                predict_arr = np.reshape(predict_arr, (1, -1))
+                predict_arr_ = np.asarray(predict_arr, dtype=float)
+                y_pred_real = model.predict(predict_arr)
+                st.markdown(
+                    f"""
+                        ### Predict: `{y_pred_real[0]}`
+                    """
+                )
+            except Exception as e:
+                st.write(e)
         else:
             st.info("Error!!!")
+else:
+    st.info("Upload dataset to continue")
